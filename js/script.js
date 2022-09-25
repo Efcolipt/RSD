82a852fedb4543ca61c3b42f4f8c3816e6bb2b9b
Ivan Baykov <efcolipt@gmail.com>
2022-09-25 16:11:50 +0300

    added about_us_in_media

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+
+[...document.getElementsByClassName('select') || []].forEach(select => {
+    select.addEventListener('click', function (_) {
+        this.classList.toggle('select--active')
+    })
+
+    Array.from(select.getElementsByClassName('select__item')).forEach(selectItem => {
+        selectItem.addEventListener('click', function (e) {
+            e.stopPropagation()
+
+            const headline = select.querySelector('.select__headline > span')
+            const headlineDefault = select.querySelector('.select__item[data-select-value="default"]').textContent
+            const val = this.getAttribute('data-select-value')
+          
+            select.setAttribute('data-select-selected', val)
+            headline.textContent = val === 'default' ?  headlineDefault : val
+
+            select.classList.toggle('select--active')
+        })
+    })
+
+});
