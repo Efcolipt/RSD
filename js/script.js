097a85489e029ed33d0dab2845c727ec6990deb0
Ivan Baykov <efcolipt@gmail.com>
2022-12-23 13:52:13 +0300

    update dev

--- js/script.js
+++ js/script.js
@@ -1,151 +1,177 @@
-
-
-$(document).ready(function(){
-
-    $('.local-news-channel__constituencies-items a').click(function () {
-        const id = $(this).attr('data-id')
-        $(this).parents('.local-news-channel__constituencies').toggleClass('local-news-channel__constituencies--open')
-        console.log(id)
+$(document).ready(function () {
+    $('.navigator__bar').click(function () {
+        $(this).parents('.navigator').toggleClass('navigator--active')
     })
 
-    $('.form__docs button').click(function (e) {
-        e.preventDefault();
-        
-        const data = $(this).parents('.form__docs').serialize()
+  $(".local-news-channel__constituencies-items a").click(function () {
+    const id = $(this).attr("data-id");
+    $(this)
+      .parents(".local-news-channel__constituencies")
+      .toggleClass("local-news-channel__constituencies--open");
+    console.log(id);
+  });
 
-        $.ajax({
-            data,
-            url: '',
-            method: 'get',
-            cache : false,
-            success: function(data){
-                $('.gos-docs__content div:first-child').html(data);
-            },
-            error: function (response) {
-                console.log(response.responseJSON.errors)
-            },
-        });
-    })
+  $(".form__docs button").click(function (e) {
+    e.preventDefault();
 
-    $('.local-news-channel__constituencies > button').click(function() {
-        $(this).parent().toggleClass('local-news-channel__constituencies--open')
-    }) 
+    const data = $(this).parents(".form__docs").serialize();
 
-
-    $('.select').click(function() {
-        $(this).toggleClass('select--active')
-    })
-
-    $('.select .select__item').click(function (e) {
-        e.stopPropagation()
-
-        const val = $(this).attr('data-select-value')
-        const select = $(this).parents('.select')
-        const headlineDefault = select.find('.select__item[data-select-value="default"]').text()
-
-        select.find('.select__headline span').text(val === 'default' ?  headlineDefault : val)
-        select.attr('data-select-selected', val)
-        select.toggleClass('select--active')
-        select.find('input').val(val)
-
-        $('.about-us-in-media').length > 0 && $.ajax({
-            dataType: "json",
-            data: {'data': val, 'count': 0},
-            url: $('.btn-default.btn-default--outline-default').attr("value"),
-            method: 'get',
-            cache : false,
-            success: function(data){
-                // interval = setTimeout(callAjax, 1000);
-                $('.about-us-in-media__list').replaceWith($(data.publications));
-                if (data.show_button === false) {
-                    $('.btn-default.btn-default--outline-default').css('display', 'none')
-                }
-                else {
-                    $('.btn-default.btn-default--outline-default').css('display', '')
-                }
-            },
-            error: function (response) {
-                console.log(response.responseJSON.errors)
-            },
-        });
-    })
-
-    
-
-    $('.header-main__nav-pages__nav-item').hover(function() {
-        $(this).toggleClass('header-main__nav-pages__nav-item--submenu-active')
-    })
-
-    $('.header-main__mobile-menu__item').click(function() {
-        $(this).toggleClass('header-main__mobile-menu__item--active')
-    })
-    
-    $('#mobile-burger').click(function() {
-        $('#mobile-menu').toggleClass('header-main__mobile-menu--active')
+    $.ajax({
+      data,
+      url: "",
+      method: "get",
+      cache: false,
+      success: function (data) {
+        $(".gos-docs__content div:first-child").html(data);
+      },
+      error: function (response) {
+        console.log(response.responseJSON.errors);
+      },
     });
-    
-    if($('[data-modal]').length > 0) 
-        new Modal({
-            linkAttributeName: 'data-modal', 
-        });
+  });
 
-    $('.slider').each(function() {
-        const count = $(this).attr('data-count') ? +$(this).attr('data-count') : 1
-        const slider = $(this).attr('data-slider')
-        const dots = $(this).attr('data-dots')
-        const responsive = []
+  $(".local-news-channel__constituencies > button").click(function () {
+    $(this).parent().toggleClass("local-news-channel__constituencies--open");
+  });
 
-        if(count === 2) {
-            responsive.push({
-                breakpoint: 720,
-                settings: {
-                    slidesToShow: 1,
-                }
-            })
-        } else if (count === 3) {
-            responsive.push({
-                breakpoint: 920,
-                settings: {
-                    slidesToShow: 2,
-                }
-            }, {
-                breakpoint: 525,
-                settings: {
-                    slidesToShow: 1,
-                }
-            })
+  $(".select").click(function () {
+    $(this).toggleClass("select--active");
+  });
+
+  $(".select .select__item").click(function (e) {
+    e.stopPropagation();
+
+    const val = $(this).attr("data-select-value");
+    const select = $(this).parents(".select");
+    const headlineDefault = select
+      .find('.select__item[data-select-value="default"]')
+      .text();
+
+    select
+      .find(".select__headline span")
+      .text(val === "default" ? headlineDefault : val);
+    select.attr("data-select-selected", val);
+    select.toggleClass("select--active");
+    select.find("input").val(val);
+
+    $(".about-us-in-media").length > 0 &&
+      $.ajax({
+        dataType: "json",
+        data: { data: val, count: 0 },
+        url: $(".btn-default.btn-default--outline-default").attr("value"),
+        method: "get",
+        cache: false,
+        success: function (data) {
+          // interval = setTimeout(callAjax, 1000);
+          $(".about-us-in-media__list").replaceWith($(data.publications));
+          if (data.show_button === false) {
+            $(".btn-default.btn-default--outline-default").css(
+              "display",
+              "none"
+            );
+          } else {
+            $(".btn-default.btn-default--outline-default").css("display", "");
+          }
+        },
+        error: function (response) {
+          console.log(response.responseJSON.errors);
+        },
+      });
+  });
+
+  $(".header-main__nav-pages__nav-item").hover(function () {
+    $(this).toggleClass("header-main__nav-pages__nav-item--submenu-active");
+  });
+
+  $(".header-main__mobile-menu__item").click(function () {
+    $(this).toggleClass("header-main__mobile-menu__item--active");
+  });
+
+  $("#mobile-burger").click(function () {
+    $("#mobile-menu").toggleClass("header-main__mobile-menu--active");
+  });
+
+  if ($("[data-modal]").length > 0)
+    new Modal({
+      linkAttributeName: "data-modal",
+    });
+
+  $(".slider").each(function () {
+    const count = $(this).attr("data-count") ? +$(this).attr("data-count") : 1;
+    const slider = $(this).attr("data-slider");
+    const dots = $(this).attr("data-dots");
+    const responsive = [];
+
+    if (count === 2) {
+      responsive.push(
+        {
+          breakpoint: 720,
+          settings: {
+            slidesToShow: 1,
+          },
+        },
+        {
+          breakpoint: 525,
+          settings: {
+            slidesToShow: 1,
+          },
         }
-
-        switch(slider){
-            case 'useful-info':
-                responsive.push({
-                    breakpoint: 525,
-                    settings: "unslick"
-                })
+      );
+    } else if (count === 3) {
+      responsive.push(
+        {
+          breakpoint: 920,
+          settings: {
+            slidesToShow: 2,
+          },
+        },
+        {
+          breakpoint: 525,
+          settings: {
+            slidesToShow: 1,
+          },
         }
+      );
+    }
 
-        $(`[data-slider="${slider}"]  .slider__items`).slick({
-            infinite: true,
-            slidesToShow: count,
-            slidesToScroll: 1,
-            prevArrow: $(`[data-slider="${slider}"]  .slider__control[data-slide="prev"]`),
-            nextArrow: $(`[data-slider="${slider}"]  .slider__control[data-slide="next"]`),
-            dots: dots === 'y' && $(`[data-slider="${slider}"]  .slider__item `).length > 2,
-            autoplay: true,
-            responsive
+    switch (slider) {
+      case "useful-info":
+        responsive.push({
+          breakpoint: 525,
+          settings: "unslick",
         });
-    })
+    }
 
-    function download(url, method) {
-        $.ajax({
-            url,
-            method,
-            dataType: 'json',
-            // data: {text: 'Текст'},
-            success: function(jsonData){
-                const parent = $(this).parents('.content-filter__content').find('.content-filter__content-other')
-                const data = JSON.parse(jsonData)
-                const structure = `
+    $(`[data-slider="${slider}"]  .slider__items`).slick({
+      infinite: true,
+      slidesToShow: count,
+      slidesToScroll: 1,
+      prevArrow: $(
+        `[data-slider="${slider}"]  .slider__control[data-slide="prev"]`
+      ),
+      nextArrow: $(
+        `[data-slider="${slider}"]  .slider__control[data-slide="next"]`
+      ),
+      dots:
+        dots === "y" &&
+        $(`[data-slider="${slider}"]  .slider__item `).length > 2,
+      autoplay: true,
+      responsive,
+    });
+  });
+
+  function download(url, method) {
+    $.ajax({
+      url,
+      method,
+      dataType: "json",
+      // data: {text: 'Текст'},
+      success: function (jsonData) {
+        const parent = $(this)
+          .parents(".content-filter__content")
+          .find(".content-filter__content-other");
+        const data = JSON.parse(jsonData);
+        const structure = `
                     <article class="card-article">
                         <img class="image" src="${data.image}" alt="">
                         <h3>${data.headline}</h3>
@@ -155,15 +181,12 @@ $(document).ready(function(){
                             </time>
                         </div>
                     </article>
-                `
-                parent.append(structure)
+                `;
+        parent.append(structure);
 
-                if($('.content-filter__content-other article').length > 0)
-                    $('.content-filter__content-other__download').css('display', 'none')
-            }
-        }); 
-    }
+        if ($(".content-filter__content-other article").length > 0)
+          $(".content-filter__content-other__download").css("display", "none");
+      },
+    });
+  }
 });
-      
-
-
